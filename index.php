a66457287367f92ca8955e0195a53cdaa7ae1ac2
yavuzkutuk <yavuz@kutuk.fr>
2024-03-27 10:52:45 +0100

    PDO

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ Vive PDO !!!
 require 'connec.php';
 $pdo = new PDO(DSN, USER, PASS);
 
-$query = "SELECT * FROM user";
-$users = $pdo->query($query)->fetchAll();
+$query = "SELECT * FROM user where name='vincent'";
+$users = $pdo->query($query)->fetch();
 
 
 foreach ($users as $name) {
